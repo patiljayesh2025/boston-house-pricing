cd5c62cd235bad3c8a7a6d9a7fb596b80ba95d9f
patiljayesh2025 <patiljayesh2025@gmail.com>
2022-09-29 21:19:00 +0530

    added flask api,html files

---
+++ main.py
@@ -0,0 +1,23 @@
+from crypt import methods
+import pickle
+from flask import Flask,request,app ,jsonify,url_for,render_template
+import numpy as np
+import pandas as pd
+
+app = Flask(__name__)
+regmodel =pickle.load(open('regmodel.pkl','rb'))
+scaler =pickle.load(open('scaling.pkl','rb'))
+
+@app.route('/',methods=['GET'])
+def home():
+    return render_template('home.html')
+
+@app.route('/predict_api',methods=['POST'])
+def predict_api():
+    data = request.json['data']
+    new_data= scaler.transform( np.array(list(data.values())).reshape(1,-1))
+    output=regmodel.predict(new_data)
+    return jsonify(output[0])
+
+if __name__=="__main__":
+    app.run(debug=True)
